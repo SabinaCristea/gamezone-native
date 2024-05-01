06c898c7cbfe812c15fab223f5486b67a331a25c
SabinaCristea <sabinacristea997@gmail.com>
2024-05-01 18:19:44 +0300

    Initial commit

---
+++ screens/about.js
@@ -0,0 +1,11 @@
+import { StyleSheet, View } from "react-native";
+
+export default function Home() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
