4464ffbda5eff1052c78e5060d8745f0e08effa2
SabinaCristea <sabinacristea997@gmail.com>
2024-05-01 19:05:36 +0300

    Created main components, added cusotm fonts

--- screens/about.js
+++ screens/about.js
@@ -1,7 +1,11 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Home() {
-  return <View style={styles.container}></View>;
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <Text>About screen</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,19 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Home screen</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     padding: 24,
+  },
+  titleText: {
+    fontFamily: "nunito-bold",
+    fontSize: 18,
   },
 });
